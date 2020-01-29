6387e599c27c46b9efcc26c75e8983f502fe8d81
TinesKumar13 <54587806+TinesKumar13@users.noreply.github.com>
2020-01-29 23:07:46 +0800

    Add files via upload

---
+++ resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Tines Kumar",
+  "role": "Computer Engineering Student",
+  "linkedinId":" Tines Kumar",
+  "skypeid": "Your skypeid",
+  "roleDescription": " and a young kid who is passionate about Software Development as well as Machine Learning and Data Science.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"http://www.linkedin.com/in/tinesh-kumar",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/TinesKumar13",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"I am a 21 year old nerd,who is passionate about learning Software as well as Hardware.",
+  "address":"Subang Jaya,Selangor",
+  "website":"tineshreds@gmail.com",
+  "education":[
+    {
+      "UniversityName":"University Technology Petronas",
+      "specialization":"B.E. Computer Engineering",
+      "YearOfPassing":"2018",
+      
+    },
+    {
+      "UniversityName":"Sunway University College",
+      "specialization":"Australian Matriculation",
+      "YearOfPassing":"2016-2017",
+      
+    },
+    {
+      "UniversityName":"Sekolah Menengah Kebangsaan Subang Jaya",
+      "YearOfPassing":"2012-2016",
+    }
+  ],
+  
+  "skillsDescription":"Skills That I am Developing",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Java"
+    },
+    {
+      "skillname":"Python"
+    },
+    {
+      "skillname":"C"
+    },
+    {
+      "skillname":"R"
+    },
+    {
+      "skillname":"Javascript"
+    },
+  ],
+  "portfolio":[
+    {
+      "name":"Java Work",
+      "description":"Algorithms And Data Structure",
+      "imgurl":"images/portfolio/phone.jpg",
+      "url":"http://github.com/TinesKumar13"
+      
+      
+      
+    },
+    {
+      "name":"Python Work",
+      "description":"Mini Apps",
+      "imgurl":"images/portfolio/project.jpg",
+      "url":"http://github.com/TinesKumar13"
+      
+    },
+    {
+      "name":"R mini-projects",
+      "description":"Data Science Tasks",
+      "imgurl":"images/portfolio/project2.png",
+      "url":"http://github.com/TinesKumar13"
+     
+    },
+    {
+      "name":"React JS",
+      "description":"My Journey On Full Stack",
+      "imgurl":"images/portfolio/phone.jpg",
+      "url":"http://github.com/TinesKumar13"
+      
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"To learn more and more technologies in this ever-growing Tech-World",
+      
+    },
+    {
+      "description":"I also love reading and learning about Machine Learning and Data Science",
+      
+    }
+  ]
+}
+
+export default resumeData
